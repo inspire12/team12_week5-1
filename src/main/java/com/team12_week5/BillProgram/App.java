98588dca0d06d0b904e40a42f866bfcb5aa81260
GwanYong Kim <kky960105@naver.com>
2016-04-08 13:56:47 +0900

    project_create
    create

---
+++ src/main/java/com/team12_week5/BillProgram/App.java
@@ -0,0 +1,13 @@
+package com.team12_week5.BillProgram;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
