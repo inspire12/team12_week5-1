e11015063255db96420ab5c7b7610e16b45cfefe
seoyeonghak <ox4443@naver.com>
2016-04-13 13:22:11 +0900

    add main

---
+++ src/main/java/real/team12/week5/Bill_main.java
@@ -0,0 +1,9 @@
+package real.team12.week5;
+
+public class Bill_main{
+	 public static void main( String[] args )
+	    {
+	        System.out.println( "Hello World!" );
+	        Calculator 
+	    }
+}
