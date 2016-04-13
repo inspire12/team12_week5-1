dcf68122bc2deb9251b86cbca06f87f712425247
seoyeonghak <ox4443@naver.com>
2016-04-13 14:32:29 +0900

    add test more, silver&gold

--- src/main/java/real/team12/week5/Bill_main.java
+++
@@ -1,9 +0,0 @@
-package real.team12.week5;
-
-public class Bill_main{
-	 public static void main( String[] args )
-	    {
-	        System.out.println( "Hello World!" );
-	        Calculator 
-	    }
-}
